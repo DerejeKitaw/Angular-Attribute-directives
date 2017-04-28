5de7f1751ffacf824c8d1f62f6ba6006f2edaa83
Dereje Kitaw <dkitaw@solareworld.local>
2017-04-28 14:31:14 -0400

    app prefix removed highlight directive updated

--- src/app/highlight.directive.ts
+++ src/app/highlight.directive.ts
@@ -1,10 +1,20 @@
-import { Directive } from '@angular/core';
+/**
+ * Directive: provides the functionality of the @Directive decorator.
+  ElementRef: injects into the directive's constructor so the code can access the DOM element.
+       Input: allows data to flow from the binding expression into the directive.
+ */
+import { Directive, ElementRef,Input } from '@angular/core';
 
 @Directive({
-  selector: '[appHighlight]'
+  //Though highlight is a more concise name than myHighlight and would work, a best practice is to prefix selector names to ensure they don't conflict with standard HTML attributes
+  //do not prefix the highlight directive name with ng because that prefix is reserved for Angular and using it could cause bugs that are difficult to diagnose. 
+  selector: '[myHighlight]'
 })
 export class HighlightDirective {
 
-  constructor() { }
+  constructor(el: ElementRef) { 
+    el.nativeElement.style.backgroundColor = 'yellow';
+    
+  }
 
 }
